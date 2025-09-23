c6f4dd92d34d111d23e56f7f16c2050adad0e77b
japupok <jakub.strhar08@gmail.com>
2025-09-23 11:12:55 +0200

    Add files via upload

---
+++ htdocs_git/config.php
@@ -0,0 +1,5 @@
+<?php
+define('WEB_Autor', 'Jakub Strhár');
+define('WEB_nadpis', 'Moja prvá stránka v PHP');
+define('Comparny_description', 'Táto stránka je o kokote.');
+?>
---
+++ htdocs_git/index.php
@@ -0,0 +1,18 @@
+<?php
+include './config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <title><?php echo WEB_nadpis; ?></title>
+    <link rel="stylesheet" href="assets/css/styl_daky.css">   
+</head>
+
+<body>
+    <h1><?php echo WEB_nadpis; ?></h1>
+    <p>Autor: <?php echo WEB_Autor; ?></p>
+    <p><?php echo Comparny_description; ?></p>
+</body>
+
+</html>
